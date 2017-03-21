fa2058dc21aa9737bef8bbd15fc3704a3518ad24
manish <arjun1177arjun@gmail.com>
2017-03-21 23:29:37 +0530

    started admin portal

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from polls.models import Question, Choice
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+  fieldsets = [
+    (None, {'fields':['question_text']}),
+    ('date information', {'fields':['pub_date'], 'classes': ['collapse']}),
+  ]
+
+  
+admin.site.register(Question, QuestionAdmin) 
+admin.site.register(Choice)  
